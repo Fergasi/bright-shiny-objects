c68584c09a15eeabfa17d6f5cc2fe141696336a4
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-02-24 22:10:53 -0500

    Changes

--- main.js
+++ main.js
@@ -2,14 +2,85 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(firstName, lastName){
+    user = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return user;
+}
 
+//-------------------------------------
 
+function setAge (user, age) {
+    user.age = age;
+    return user
+}
 
+//-------------------------------------
 
+function incrementAge(user) {
+    user.age = user.age + 1
+    return user
+}
 
+//-------------------------------------
 
+function fixCar(car){
+    car.needsMaintenance = false
+    return car
+}
+ 
+//-------------------------------------
 
+function addGrades(student, newGrades) {
 
+    for (let count = 0; count < newGrades.length; count++)
+    student.grades.push(newGrades[count])
+
+    return student
+}
+
+//-------------------------------------
+
+function getDataType(object, key){
+    return typeof object[key];
+}
+
+//-------------------------------------
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo)
+    return todos
+}
+
+//-------------------------------------
+
+function addSong(playlist, newSong){
+    playlist.songs.push(newSong)
+    playlist.duration += newSong.duration
+
+    return playlist
+}
+
+//-------------------------------------
+
+function updateReportCard(reportCard, newGrade) {
+
+    reportCard.grades.push(newGrade)
+    reportCard.lowestGrade = Math.min(...reportCard.grades)
+    reportCard.highestGrade = Math.max(...reportCard.grades)
+    
+    let sum = 0;
+    for (i=0; i < reportCard.grades.length; i++){
+        sum += reportCard.grades[i]
+    }
+
+    reportCard.averageGrade = sum / reportCard.grades.length
+
+    return reportCard
+
+}
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
